a45d6ef1ed9365a8e310572993d4f9046be39dfc
gauron99 <fridrich.david19@gmail.com>
2022-02-11 16:22:24 +0100

    cli parameter works, Writing re-done -> using its own function

--- http-server/main.go
+++ http-server/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
-	"net/http"
-	"strings"
+	"math/rand" // random number generator
+	"net/http"  //web stuff
+	"os"
+	"strconv" //string conversion
+	"strings" //string manipulation
 	"time"
 )
 
@@ -12,6 +14,14 @@ var greetings_array = []string{"Hello", "Greetings", "Welcome", "Hi"}
 var titles_array = []string{"the Mighty Traveler", "the Great Summoner", "the Conqueror of Titans", "the Destroyer of Worlds", "the King of Oceans", "the King of Underworld"}
 
 var page_access_count = 0
+var param = ""
+
+func webWriter(rw http.ResponseWriter, s string) {
+	fmt.Fprintf(rw, "%s\n", s)
+	if len(param) > 0 { //if a parameter is given, print it out everywhere where something is printed out
+		fmt.Fprintf(rw, "I have a parameter! Here: %s\n", param)
+	}
+}
 
 // sayHello is a handler for api call "/hello"
 // Its possible to give a 'name' parameter as "/hello?name=John"
@@ -22,43 +32,81 @@ func sayHello(rw http.ResponseWriter, req *http.Request) {
 	if strings.Contains(parser, "name=") { //greet a person if "name" parameter is given
 		name := parser[12:]
 		if len(name) == 0 { //greet "nobody"
-			fmt.Fprintf(rw, "Greetings Mr. Nobody!\n")
+			webWriter(rw, "Greetings Mr. Nobody!")
 		} else { // greet specific person
-			fmt.Fprintf(rw, "%s %s, %s!\n", greetings_array[rand.Intn(len(greetings_array))], name, titles_array[rand.Intn(len(titles_array))])
+			webWriter(rw, greetings_array[rand.Intn(len(greetings_array))]+" "+name+" "+titles_array[rand.Intn(len(titles_array))])
 		}
 	} else { //greet generic
-		fmt.Fprintf(rw, "Hello there! This page has been accessed %dx times\n", page_access_count)
+		webWriter(rw, "Hello there! This page has been accessed "+strconv.FormatInt(int64(page_access_count), 10)+"x times\n")
 	}
 
 	page_access_count += 1
+
 }
 
-func api_root(rw http.ResponseWriter, req *http.Request) {
-	// start ticker
-	// tick_sec := time.NewTicker(time.Second) //tick every second to show current time
-	// done_sec := make(chan bool)
-	// go func() {
-	// 	for {
-	// 		select {
-	// 		case <-done_sec:
-	// 			return
-	// 		case t := <-tick_sec.C:
-	// 			// Layouts must use the reference time Mon Jan 2 15:04:05 MST 2006 to show the pattern with which to format/parse a given time/string.
-	// 			// display_time(t, rw)
-	// 			fmt.Fprint(rw, "Now: ", t.Format("2006-01-02 15:04:05"))
-	// 		}
-	// 	}
-	// }()
+// TODO
+// func getCurrTime() {
+// }
 
+func apiHome(rw http.ResponseWriter, req *http.Request) {
+	// start ticker
+	// tick_sec := time.NewTicker(1 * time.Second) //tick every second to show current time
+	// for {
+	// 	select {
+	// 	case t := <-tick_sec.C:
+	// 		fmt.Fprintf(rw, t.Format("2006-01-02 15:04:05"))
+	// 	}
+	// }
 	t := time.Now()
-	fmt.Fprint(rw, "Now: ", t.Format("2006-01-02 15:04:05"))
+	fmt.Println("Now: ", t.Format("2006-01-02 15:04:05"))
+}
+
+// --- Parse cli arguments ---
+// Only looking for long: "parameter" or "param" / short: "p"
+// Can be given with "=" or as following argument
+// 		ex: "--param=hello" or "--param hello" or "-p=hello"
+// Ignores unknown arguments
+func argParser() {
+	if len(os.Args) > 1 {
+		args := os.Args[1:]
+		capture_para := false
+
+		for _, item := range args { //parse each argument
+			if capture_para == true {
+				param = item
+				capture_para = false
+
+			} else if strings.HasPrefix(item, "--parameter") || strings.HasPrefix(item, "--param") || strings.HasPrefix(item, "-p") {
+				if strings.Contains(item, "=") { //if parameter contains '=' -> extract the argument
+					item = item[strings.IndexByte(item, '='):][1:] //cut string until '=' (exluding =)
+
+					if len(item) > 0 { //if len is 0 do nothing
+						param = item
+					}
+				} else {
+					capture_para = true // capture next parameter
+				}
+
+			} else {
+				fmt.Println("Unknown parameter '", item, "' given, ignoring.")
+			}
+		}
+	}
 }
 
 func main() {
-	fmt.Println("Starting serer...")
 
-	http.HandleFunc("/", api_root)
-	http.HandleFunc("/hello", say_hello)
+	// handle possible cli arguments & set
+	argParser()
+
+	if param != "" {
+		fmt.Println("I have a parameter!", param)
+	}
+	fmt.Println("Server started...")
+	defer fmt.Println("Server closed. Bye") //print at the end? need to catch signal to properly end main() probably
+
+	http.HandleFunc("/", apiHome)
+	http.HandleFunc("/hello", sayHello)
 
 	http.ListenAndServe(":8000", nil)
 

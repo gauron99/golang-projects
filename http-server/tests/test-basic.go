3a5d783917a9a5328bde5502a932393fda368c88
gauron99 <fridrich.david19@gmail.com>
2022-02-16 11:20:36 +0100

    sync

---
+++ http-server/tests/test-basic.go
@@ -0,0 +1,12 @@
+package main
+
+// import "testing"
+
+// func TestOne(t *testing.T) {
+// 	want := ""
+// 	// if got := t.FuncCall(); got != want {
+// 		t.Errorf("FuncCall() big bad => %q, want %q", got, want)
+// 	}
+// }
+
+// // use "go test" in cli to use

4f48c31a1ab9cb404d5ac8690463c80950ac987a
gauron99 <fridrich.david19@gmail.com>
2022-03-09 16:30:01 +0100

    testing all 3 parameters (input,param,env vars)

--- http-server/server/server_test.go
+++ http-server/server/server_test.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"os"
@@ -13,44 +14,107 @@ import (
 ///////////////////////////////////////////////////////////////////////////////
 type webWriterMetadataTestStruct struct {
 	name     string
-	metadata map[string]string //input, parameter, environment var
+	metadata map[string]interface{} //input, parameter, environment var(map)
 	isError  bool
 }
 
 var testsWebWriterMetaData = []webWriterMetadataTestStruct{
 	{
 		name: "nothing passed, should pass",
-		metadata: map[string]string{
-			"input":  "",
-			"param":  "",
-			"envVar": "",
+		metadata: map[string]interface{}{
+			"input": "",
+			"param": "",
+			"envVar": map[string]string{
+				"VAR": ""},
 		},
 		isError: false,
 	},
 	{
 		name: "just input given, should pass",
-		metadata: map[string]string{
-			"input":  "input written",
-			"param":  "",
-			"envVar": "",
+		metadata: map[string]interface{}{
+			"input": "input written",
+			"param": "",
+			"envVar": map[string]string{
+				"VAR": ""},
+		},
+		isError: false,
+	},
+	{
+		name: "param given, should pass",
+		metadata: map[string]interface{}{
+			"input": "",
+			"param": "param given",
+			"envVar": map[string]string{
+				"VAR": ""},
 		},
 		isError: false,
 	},
 	{
 		name: "input + param given, should pass",
-		metadata: map[string]string{
-			"input":  "input written",
-			"param":  "param given",
-			"envVar": "",
+		metadata: map[string]interface{}{
+			"input": "input written",
+			"param": "param given",
+			"envVar": map[string]string{
+				"VAR": ""},
+		},
+		isError: false,
+	},
+	{
+		name: "envVar given, should pass",
+		metadata: map[string]interface{}{
+			"input": "",
+			"param": "",
+			"envVar": map[string]string{
+				"VAR": "envVar given"},
+		},
+		isError: false,
+	},
+	{
+		name: "input + envVar given, should pass",
+		metadata: map[string]interface{}{
+			"input": "input written",
+			"param": "",
+			"envVar": map[string]string{
+				"VAR": "envVar given"},
+		},
+		isError: false,
+	},
+	{
+		name: "param + envVar given, should pass",
+		metadata: map[string]interface{}{
+			"input": "",
+			"param": "param given",
+			"envVar": map[string]string{
+				"VAR": "envVar given"},
+		},
+		isError: false,
+	},
+	{
+		name: "input + param + envVar given, should pass",
+		metadata: map[string]interface{}{
+			"input": "input written",
+			"param": "param given",
+			"envVar": map[string]string{
+				"VAR": "envVar given"},
+		},
+		isError: false,
+	},
+	{
+		name: "input + param + envVar (multiple) given, should pass",
+		metadata: map[string]interface{}{
+			"input": "input written",
+			"param": "param given",
+			"envVar": map[string]string{
+				"VAR": "hello",
+				"BAR": "2",
+				"FOO": "interface not really"},
 		},
 		isError: false,
 	},
 }
 
 // TestWebWriter tests webWriter function with & without parameter
-// (does not use interfaces)
 func TestWebWriter(t *testing.T) {
-	os.Clearenv()
 	s := NewServerInfo("")
 
 	paramPre := "I have a parameter! Here: "
@@ -59,26 +123,33 @@ func TestWebWriter(t *testing.T) {
 	for _, data := range testsWebWriterMetaData {
 		t.Run(data.name, func(t *testing.T) {
 			res := httptest.NewRecorder()
-			s.param = data.metadata["param"]
+			// set parameter
+			s.param = data.metadata["param"].(string)
 
-			s.webWriter(res, data.metadata["input"])
+			// set environment vars
+			os.Clearenv()
+			for k, v := range data.metadata["envVar"].(map[string]string) {
+				os.Setenv(k, v)
+			}
+
+			s.webWriter(res, data.metadata["input"].(string))
 			if res.Result().StatusCode != http.StatusOK {
 				t.Error("Expected status '200 OK' but got:", res.Result().Status)
 			}
 
 			exp := ""
 			if data.metadata["input"] != "" {
-				exp += data.metadata["input"] + "\n"
+				exp += data.metadata["input"].(string) + "\n"
 			}
 			if data.metadata["param"] != "" {
-				exp += paramPre + data.metadata["param"] + "\n"
+				exp += paramPre + data.metadata["param"].(string) + "\n"
 			}
-			if data.metadata["envVar"] != "" {
-				exp += envVarPre + data.metadata["envVar"] + "\n"
+			if len(data.metadata["envVar"].(map[string]string)) > 0 {
+				exp += fmt.Sprintf("%s%v\n", envVarPre, data.metadata["envVar"].(map[string]string))
 			}
 
 			if res.Body.String() != exp {
-				t.Error("data printed out (", res.Body.String(), ") does not match the expected (", exp, ")")
+				t.Errorf("got |%s|, expected |%s|", res.Body.String(), exp)
 			}
 
 		})

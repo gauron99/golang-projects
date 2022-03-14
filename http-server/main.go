cebcd546a174c659c992d0b5562e24cbfe442f64
David Fridrich <49119790+gauron99@users.noreply.github.com>
2022-03-14 14:23:59 +0100

    Testing (#13)
    * env vars exist in 'env' file and are printed out
    
    * test works for apiHome
    
    * testing all 3 parameters (input,param,env vars)
    
    * err handling stuff

--- http-server/main.go
+++ http-server/main.go
@@ -2,21 +2,62 @@ package main
 
 import (
 	// "html/template" // for later use with html
+	"bufio"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"strings"
 
 	"server/server"
 )
 
+// loadEnvSettings reads data from a file called "env" in current dir
+// and sets all environment variables for this run.
+// Returns slice of keys in the env file.
+func loadEnvSettings() (err error) {
+
+	//clear all pre-set variables since we dont need any
+	os.Clearenv()
+	f, err := os.Open("./env")
+	if err != nil {
+		return err
+	}
+	defer f.Close() // close when func is done
+
+	scanner := bufio.NewScanner(f)
+
+	// cycle through each line
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.HasPrefix(line, "#") {
+			continue //lines starting with '#' are comments
+		}
+		if !strings.Contains(line, "=") { //line has to contain '='
+			log.Println("Warning: env_file wasn't properly loaded; line in env file doesn't contain '=':", line)
+			return nil
+		}
+		split := strings.Split(line, "=")
+		os.Setenv(split[0], split[1])
+	}
+	return nil
+}
+
 func main() {
+	e := loadEnvSettings()
+	if e != nil {
+		log.Fatal(e) //print out error and exit
+	}
+
+	os.Setenv("HARDCODED", "nejvic") //custom env var
+
 	var paramPtr string
 	flag.StringVar(&paramPtr, "param", "", "print this everywhere(long)")
 	flag.StringVar(&paramPtr, "p", "", "print this everywhere(short)")
-
 	// handle possible cli arguments & set
 	flag.Parse()
+
 	serv := server.NewServerInfo(paramPtr)
 
 	fmt.Println("Server started...")
--- http-server/server/server.go
+++ http-server/server/server.go
@@ -3,9 +3,11 @@ package server
 import (
 	"fmt"
 	"io"
+	"log"
 	"math/rand"
 	"net/http"
 	"net/url"
+	"os"
 	"strconv"
 	"strings"
 	"time"
@@ -15,6 +17,16 @@ import (
 type serverInfo struct {
 	pageAccessCount int
 	param           string
+}
+
+// getEnvVars reads all env variables and returns them as a map (foo[key] = val)
+func getEnvVars() map[string]string {
+	res := make(map[string]string)
+	for _, env := range os.Environ() {
+		pair := strings.Split(env, "=")
+		res[pair[0]] = pair[1]
+	}
+	return res
 }
 
 // MakeServerInfo creates & returns newly initialized serverInfo structure with given args
@@ -29,12 +41,27 @@ var titles = []string{"the Mighty Traveler", "the Great Summoner", "the Conquero
 // additionally if ENV variable or CLI parameter are given, print those too
 func (si serverInfo) webWriter(rw http.ResponseWriter, s string) {
 	if s != "" {
-		fmt.Fprintf(rw, "%s\n", s)
+		outS := fmt.Sprintf("%s\n", s)
+		_, err := io.WriteString(rw, outS)
+		if err != nil {
+			log.Printf("Error while writing string: %s", err)
+		}
 	}
-	if len(si.param) > 0 { //if a parameter is given, print it out everywhere where something is printed out
 
-		out := fmt.Sprintf("I have a parameter! Here: %s\n", si.param)
-		io.WriteString(rw, out)
+	if par := si.param; len(par) > 0 { //if a parameter is given, print it out everywhere where something is printed out
+
+		outParam := fmt.Sprintf("I have a parameter! Here: %s\n", par)
+		_, err := io.WriteString(rw, outParam)
+		if err != nil {
+			log.Printf("Error while writing paramaters: %s", err)
+		}
+	}
+	if vars := getEnvVars(); len(vars) > 0 {
+		outVars := fmt.Sprintf("\nMy environment variables: %v\n", vars)
+		_, err := io.WriteString(rw, outVars)
+		if err != nil {
+			log.Printf("Error while writing environment variables: %s", err)
+		}
 	}
 }
 
@@ -46,7 +73,8 @@ func (si *serverInfo) SayHello(rw http.ResponseWriter, req *http.Request) {
 	URL := req.URL.String()
 	u, err := url.Parse(URL) //parsed url
 	if err != nil {
-		panic(err)
+		log.Printf("Error while parsing URL: %s", err)
+		rw.WriteHeader(500)
 	}
 	// parse query
 	args, _ := url.ParseQuery(u.RawQuery)
@@ -80,6 +108,8 @@ func (si serverInfo) ApiHome(rw http.ResponseWriter, req *http.Request) {
 	// 		fmt.Fprintf(rw, t.Format("2006-01-02 15:04:05"))
 	// 	}
 	// }
-	t := time.Now()
-	fmt.Println("Now: ", t.Format("2006-01-02 15:04:05"))
+	out := time.Now().Format("2006-01-02 15:04:05")
+	// out = strings.TrimSuffix(out, "\n")
+	// fmt.Println("LALA", out)
+	si.webWriter(rw, out)
 }
